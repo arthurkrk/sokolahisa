f4f0604c94bf7da7e74ab25d1dcea880344e5cfc
F1 <fmmirza@iuj.ac.jp>
2024-11-19 13:12:25 +0900

    app.py

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ import yfinance as yf
 import datetime
 from datetime import date, timedelta
 from datetime import datetime
+from matplotlib.ticker import FormatStrFormatter
 from textblob import TextBlob  # Import TextBlob for sentiment analysis
 ad.user_cache_dir = lambda *args: "/tmp"
 #Specify title and logo for the webpage.
@@ -337,12 +338,13 @@ with tabs[3]:
                 plt.rcParams['axes.facecolor'] = '#EAEAF2'  
                 plt.rcParams['figure.facecolor'] = '#FFFFFF'
                 
-                fig, ax = plt.subplots(figsize=(8,6))
+                fig, ax = plt.subplots(figsize=(10,8))
                 sc = ax.scatter(returns.std(), returns.mean(), s=100, alpha=0.7, edgecolors="k", c = returns.mean(), cmap = 'coolwarm', marker = 'o')
                 for stock, std, mean in zip(returns.columns, returns.std(), returns.mean()):
-                    ax.annotate(stock, (std, mean), textcoords="offset points", xytext=(2,2), ha='left')
+                    ax.annotate(stock, (std, mean), textcoords="offset points", xytext=(1,1), ha='right')
                 ax.set(title="Risk-Return Map", xlabel="Risk (Standard Deviation)", ylabel="Expected Returns (Mean)")
-
+                ax.xaxis.set_major_formatter(FormatStrFormatter('%.2f')) 
+                ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
                 cb = plt.colorbar(sc)
                 cb.set_label('Expected Returns')
                 st.pyplot(fig)
@@ -352,7 +354,7 @@ with tabs[3]:
                 st.write("### Stock Price Correlation")
                 fig, ax = plt.subplots(figsize=(6, 4))
                 sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm", fmt=".2f", linewidths=0.5, ax=ax)
-                ax.set_title("Correlation Matrix", fontsize=16)
+                ax.set_title("Correlation Matrix", fontsize=12)
                 st.pyplot(fig)
 
                 # Pairwise Scatter Plots
@@ -360,7 +362,7 @@ with tabs[3]:
                 for i, stock_x in enumerate(stocks):
                     for j, stock_y in enumerate(stocks):
                         if i < j:
-                            fig, ax = plt.subplots(figsize=(4, 3))
+                            fig, ax = plt.subplots(figsize=(10, 8))
                             sns.scatterplot(x=data[stock_x], y=data[stock_y], alpha=0.7, ax=ax)
                             ax.set_title(f"Correlation: {stock_x} vs {stock_y} ({correlation_matrix.loc[stock_x, stock_y]:.2f})")
                             ax.set_xlabel(stock_x)
